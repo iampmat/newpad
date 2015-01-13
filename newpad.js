50cd218d68a4ae3835a1c07b83964263a746150e
iampmat <patrickmatherly1994@gmail.com>
2015-01-13 11:17:20 -0500

    added date

--- newpad.js
+++ newpad.js
@@ -1,9 +1,29 @@
 //Check edits onload
-window.addEventListener('load', checkEdits);
+window.addEventListener('load', onLoad);
+function onLoad() {
+	var editElem = document.getElementById("edit");
+	var today = new Date();
+	var dd = today.getDate();
+	var mm = today.getMonth()+1; //January is 0!
+	var yyyy = today.getFullYear();
+
+	if(dd<10) {
+    	dd='0'+dd
+	} 
+
+	if(mm<10) {
+    	mm='0'+mm
+	} 
+
+	today = mm+'/'+dd+'/'+yyyy;
+
+	editElem.innerHTML += today;
+	checkEdits();
+}
 function checkEdits() {
 	//find out if the user has previously saved edits
 	if(localStorage.userEdits!=null) {
-		document.getElementById("edit").innerHTML = localStorage.userEdits;
+		document.getElementById("edit").innerHTML += localStorage.userEdits;
 	}
 };
 document.onkeyup = function (e) {
